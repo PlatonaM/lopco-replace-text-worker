1ee6736a232a4e610ee084812e4c297baecf0afc
y-du <42994541+y-du@users.noreply.github.com>
2020-11-27 14:57:12 +0100

    init

---
+++ replacer.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+#   Copyright 2020 InfAI (CC SES)
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+
+# Environment variables:
+#
+# $DEP_INSTANCE
+# $JOB_CALLBACK_URL
+# $input_file
+# $old_text
+# $new_text
+
+
+out_file_name="$(cat /proc/sys/kernel/random/uuid | echo $(read s; echo ${s//-}))"
+
+cd /data_cache
+
+echo "replacing text ..."
+sed_string="s/$old_text/$new_text/g"
+if sed $sed_string "$input_file" > "$out_file_name"; then
+    head -5 "$out_file_name"
+    echo "total number of lines written:" $(( $(wc -l < "$out_file_name") - 1 ))
+    if ! curl -s -S --header 'Content-Type: application/json' --data "{\""$DEP_INSTANCE"\": {\"output_file\": \""$out_file_name"\"}}" -X POST "$JOB_CALLBACK_URL"; then
+        echo "callback failed"
+        rm "$out_file_name"
+    fi
+else
+    echo "replacing text failed"
+fi
